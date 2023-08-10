95944d397dd632e787237766c181018ac31facbc
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2023-08-10 09:33:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+});
 
+// Event listener for the left button
+leftButton.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + assignments.length) % assignments.length;
+    displayAssignment(currentIndex);
+});
+
+// Initial display
+displayAssignment(currentIndex);
