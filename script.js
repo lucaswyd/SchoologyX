4c1ce175a41f50dab7562df4ca6cf034f7b7ee13
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2023-08-20 15:11:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,111 @@ displayAssignments();
 seeMoreButton.addEventListener("click", () => {
     assignmentBox.style.flexWrap = "wrap"; // Adjust flex-wrap property to wrap assignments
 });
+
+// Function to display assignment details overlay
+function displayAssignmentOverlay(assignment) {
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+
+    const assignmentDetails = document.createElement("div");
+    assignmentDetails.classList.add("assignment-details");
+
+    assignmentDetails.innerHTML = `
+        <h3>${assignment.name}</h3>
+        <p>${assignment.description}</p>
+        <p>Due: ${assignment.dueDate}</p>
+    `;
+
+    overlay.appendChild(assignmentDetails);
+    document.body.appendChild(overlay);
+
+    overlay.addEventListener("click", () => {
+        document.body.removeChild(overlay);
+    });
+}
+
+// Get a reference to the assignment box and the slider buttons
+const assignmentBox = document.querySelector(".assignment-box");
+const leftButton = document.querySelector(".slider-button-left");
+const rightButton = document.querySelector(".slider-button-right");
+const seeMoreButton = document.querySelector(".see-more-button"); // Add reference to the "See All" button
+
+let currentIndex = 0;
+
+// Function to display all assignments
+function displayAssignments() {
+    console.log("Displaying assignments"); // Debug log
+
+    assignmentBox.innerHTML = ''; // Clear existing assignments
+
+    assignments.forEach((assignment) => {
+        console.log("Adding assignment:", assignment.name); // Debug log
+        assignmentBox.innerHTML += `
+            <div class="assignment">
+                <h3>${assignment.name}</h3>
+                <p>Due: ${assignment.dueDate}</p>
+            </div>
+        `;
+    });
+}
+
+// Greetings based on time of day
+const currentHour = new Date().getHours();
+let greeting = "";
+if (currentHour >= 0 && currentHour < 7) {
+    greeting = "Rise and Shine";
+} else if (currentHour >= 7 && currentHour < 12) {
+    greeting = "Good Morning";
+} else if (currentHour >= 12 && currentHour < 18) {
+    greeting = "Good Afternoon";
+} else {
+    greeting = "Good Evening";
+}
+
+// Display the greeting
+document.getElementById("greeting").textContent = `${greeting}, Lucas!`;
+
+// Event listener for the right button
+rightButton.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % assignments.length;
+    displayAssignments();
+});
+
+// Event listener for the left button
+leftButton.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + assignments.length) % assignments.length;
+    displayAssignments();
+});
+
+// Initial display
+displayAssignments();
+
+// Event listener for the "See All" button
+seeMoreButton.addEventListener("click", () => {
+    assignmentBox.style.flexWrap = "wrap"; // Adjust flex-wrap property to wrap assignments
+});
+
+// Event listener for assignment boxes
+assignmentBox.addEventListener("click", (event) => {
+    const assignmentElement = event.target.closest(".assignment");
+    if (assignmentElement) {
+        const index = Array.from(assignmentElement.parentNode.children).indexOf(assignmentElement);
+        const assignment = assignments[index];
+        displayAssignmentOverlay(assignment);
+    }
+});
+
+// Initialize Particle.js animation
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        // Add more particle configuration options here
+    }
+});
+
