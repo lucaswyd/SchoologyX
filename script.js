4416b545339cf92e44bf93e4fffde2a9de7d99cf
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2023-08-30 09:28:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,17 @@ particlesJS("particles-js", {
         // Add more particle configuration options here
     }
 });
+
+const fullscreenButton = document.getElementById("fullscreen-button");
+
+fullscreenButton.addEventListener("click", () => {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
+});
